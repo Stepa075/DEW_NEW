e214b374bee818a220a061876b48aa667c64e6e3
Stepa075 <stepa0751@gmail.com>
2022-11-12 03:47:47 +0200

    Notes_synk server is ready!

--- Python/Notes_sync/server/server.py
+++ Python/Notes_sync/server/server.py
@@ -41,16 +41,22 @@ app = Flask(__name__)
 
 @app.route("/data_set", methods=['GET', ])
 def set_info():
-    data = request.args.get('data')  # Получаем значение ID из запроса
-    incoming_list = list(data.split(','))
-    for lis in incoming_list:
-        if lis not in Variables.list_of_content_data: Variables.list_of_content_data.append(lis)
-    for lis in incoming_list:
-        if lis not in Variables.list_of_content_txt: Variables.list_of_content_txt.append(lis)
-    print(Variables.list_of_content_data)
 
-    with open('../data.pickle', 'wb') as f:
-        pickle.dump(Variables.list_of_content_data, f)
+    data = request.args.get('data')  # Получаем значение ID из запроса
+    if data != '':
+        incoming_list = list(data.split(','))
+        for lis in incoming_list:
+            if lis not in Variables.list_of_content_data: Variables.list_of_content_data.append(lis)
+        for lis in incoming_list:
+            if lis not in Variables.list_of_content_txt: Variables.list_of_content_txt.append(lis)
+        print(Variables.list_of_content_data)
+
+        with open('data/data.pickle', 'wb') as f:
+            pickle.dump(Variables.list_of_content_data, f)
+    else:
+        Variables.list_of_content_data = []
+        with open('data/data.pickle', 'wb') as f:
+            pickle.dump(Variables.list_of_content_data, f)
     return "Ok!"
 
 
